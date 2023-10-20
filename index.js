ef0214d6b757de1c6bd531d5dd49392fd6190502
louis-Philippe <louis_Philippe@outlook.com>
2023-10-20 14:14:22 -0400

    routes OKAY

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use("/api/converter", require("./routes/api/converter"));
+app.use("/api", require("./routes/api/converter.js"));
 
-app.get("/", (req, res) => {
-  res.send("Home sweet home");
+app.all("/*", (req, res) => {
+  res.status(404).send("La route n'existe pas!");
 });
 
 app.listen(80, () => console.log("Server started"));
--- routes/api/converter.js
+++ routes/api/converter.js
@@ -1,27 +1,113 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("Need a POST request plz!");
+// Longueur: Conversion de pieds à mètres et mètres à pieds
+router.post("/longueur/pieds-en-metres", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const metres = parseFloat(valeur) * 0.3048;
+    res.send(`Valeur convertie: ${metres} mètres.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
 });
 
-router.post("/", async (req, res) => {
-  // Extract type and value from the request body
-  const { type, value } = req.body;
-  // Check if the type is "feet2meter" and the value is a valid number
-  if (type.toLowerCase() === "feet2meter" && !isNaN(value)) {
-    // log to server console for debugging
-    console.log("received data: " + req.body.type + " for " + req.body.value);
-    // Convert value from feet to meters (1 foot = 0.3048 meters)
-    const meters = parseFloat(value) * 0.3048;
-    // Log the received data and the converted value
-    console.log(`Received data: ${value} feet. Converted to ${meters} meters.`);
-    // Send the converted value in the response
-    res.send(`Converted value: ${meters} meters.`);
+router.post("/longueur/metres-en-pieds", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const pieds = parseFloat(valeur) / 0.3048;
+    res.send(`Valeur convertie: ${pieds} pieds.`);
   } else {
-    // If the type is defined or the value is not a valid number, send an error response
-    res.status(400).send("Invalid input.");
+    res.status(400).send("Entrée invalide.");
   }
+});
+
+// Masse: Conversion de kilogrammes à livres et livres à kilogrammes
+router.post("/masse/kilogrammes-en-livres", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const livres = parseFloat(valeur) * 2.20462;
+    res.send(`Valeur convertie: ${livres} livres.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+router.post("/masse/livres-en-kilogrammes", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const kilogrammes = parseFloat(valeur) / 2.20462;
+    res.send(`Valeur convertie: ${kilogrammes} kilogrammes.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+// Température: Conversion de Celsius à Fahrenheit et Fahrenheit à Celsius
+router.post("/temperature/celsius-en-fahrenheit", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const fahrenheit = (parseFloat(valeur) * 9) / 5 + 32;
+    res.send(`Valeur convertie: ${fahrenheit} °F.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+router.post("/temperature/fahrenheit-en-celsius", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const celsius = ((parseFloat(valeur) - 32) * 5) / 9;
+    res.send(`Valeur convertie: ${celsius} °C.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+// Vitesse: Conversion de kilomètres par heure à miles par heure et miles par heure à kilomètres par heure
+router.post("/vitesse/kilometres-par-heure-en-miles-par-heure", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const mph = parseFloat(valeur) * 0.621371;
+    res.send(`Valeur convertie: ${mph} mph.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+router.post("/vitesse/miles-par-heure-en-kilometres-par-heure", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const kmph = parseFloat(valeur) / 0.621371;
+    res.send(`Valeur convertie: ${kmph} km/h.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+// Volume: Conversion de litres à gallons et gallons à litres
+router.post("/volume/litres-en-gallons", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const gallons = parseFloat(valeur) * 0.264172;
+    res.send(`Valeur convertie: ${gallons} gallons.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+router.post("/volume/gallons-en-litres", (req, res) => {
+  const { valeur } = req.body;
+  if (valeur && !isNaN(valeur)) {
+    const litres = parseFloat(valeur) / 0.264172;
+    res.send(`Valeur convertie: ${litres} litres.`);
+  } else {
+    res.status(400).send("Entrée invalide.");
+  }
+});
+
+router.all("/*", (req, res) => {
+  res.status(404).send("La route n'existe pas!");
 });
 
 module.exports = router;
